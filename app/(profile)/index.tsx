825969f10c330660f3d713401701d257681d5883
Jakiro12 <eclipsisnocte@gmail.com>
2025-10-01 21:19:30 -0300

    done

--- app/(profile)/index.tsx
+++ app/(profile)/index.tsx
@@ -1,29 +1,63 @@
-import { Text, TouchableOpacity, View } from "react-native"
+import { ScrollView, Text, TouchableOpacity, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import BottomNavBar from "../components/navbar";
 import styles from '../../stylesScreens/profile-styles'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const ProfileDashboard=()=>{
     return(
     <SafeAreaView style={{ flex: 1, backgroundColor: "black" }} edges={["bottom", "top"]}>
         <View style={styles.container}>            
-            <View style={{width:'90%',borderWidth:1,borderColor:'#000000',height:'10%',display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+            <View style={styles.headerContainer}>
                 <Text style={{fontSize:24}}>
                     Account
                 </Text>
                 <FontAwesome name="bell" size={24} color="#fad312ff" />
             </View>
-            <View style={{width:'90%',borderWidth:1,borderColor:'#000000',height:'80%',display:'flex',flexDirection:'column'}} >
+            <View style={styles.mainOptionsContainer} >
+                <ScrollView 
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{width:'100%',display:'flex',flexDirection:'column',justifyContent:'flex-start',alignItems:'center'}}>
                 <View style={{width:'100%',height:'auto'}}>
-                    <TouchableOpacity>
-                        <Text>My reservation</Text>
+                    <TouchableOpacity style={styles.btnActionToSetInfo}>
+                        <Text style={styles.textBtnAction}>My reservation</Text>
+                        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
                     </TouchableOpacity>    
+                    <TouchableOpacity style={styles.btnActionToSetInfo}>
+                        <Text style={styles.textBtnAction}>My RSVPs</Text>
+                        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+                    </TouchableOpacity>    
+                    <TouchableOpacity style={styles.btnActionToSetInfo}>
+                        <Text style={styles.textBtnAction}>My Credits</Text>
+                        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+                    </TouchableOpacity> 
+                    <TouchableOpacity style={styles.btnActionToSetInfo}>
+                        <Text style={styles.textBtnAction}>My Membership</Text>
+                        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+                    </TouchableOpacity> 
+                    <TouchableOpacity style={styles.btnActionToSetInfo}>
+                        <Text style={styles.textBtnAction}>Hotel Bookings</Text>
+                        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+                    </TouchableOpacity>    
+                    <TouchableOpacity style={styles.btnActionToSetInfo}>
+                        <Text style={styles.textBtnAction}>Favourites</Text>
+                        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+                    </TouchableOpacity> 
                 </View>
-                <TouchableOpacity>
-                    <Text>Email & Password</Text>
-                </TouchableOpacity>
-
+                <View style={{width:'100%',height:'auto'}}>
+                    <Text style={{fontSize:15,marginBottom:10,marginTop:5}}>ACCOUNT SETTINGS</Text>
+                    <TouchableOpacity style={styles.btnActionToSetInfo}>
+                        <Text style={styles.textBtnAction}>Email</Text>
+                        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+                    </TouchableOpacity> 
+                    <TouchableOpacity style={styles.btnActionToSetInfo}>
+                        <Text style={styles.textBtnAction}>Password</Text>
+                        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+                    </TouchableOpacity> 
+                </View> 
+                               
+                </ScrollView>
             </View>
             <BottomNavBar/>
         </View>
--- stylesScreens/profile-styles.ts
+++ stylesScreens/profile-styles.ts
@@ -2,7 +2,10 @@ import { StyleSheet,ImageStyle,ViewStyle,TextStyle } from "react-native";
 
 interface InitScreen{
     container:ViewStyle
-   
+    headerContainer:ViewStyle
+    mainOptionsContainer:ViewStyle
+    btnActionToSetInfo:ViewStyle
+    textBtnAction:TextStyle
 }
 
 const ProfileStyles : InitScreen = StyleSheet.create({
@@ -15,6 +18,36 @@ const ProfileStyles : InitScreen = StyleSheet.create({
         flexDirection:'column',
         backgroundColor:'#e0e3e9'
     },
+    headerContainer:{
+        width:'90%',
+        borderWidth:1,
+        borderColor:'#000000',
+        height:'10%',
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    mainOptionsContainer:{
+        width:'90%',   
+        height:'80%',
+        display:'flex',
+        flexDirection:'column'
+    },
+    btnActionToSetInfo:{
+        width:'100%',
+        height:35,
+        borderBottomWidth:1,
+        borderBottomColor:'#6e8ac6',
+        display:'flex',
+        justifyContent:'space-between',
+        alignItems:'center',
+        flexDirection:'row'
+    },
+    textBtnAction:{
+        fontSize:16,
+        fontWeight:'bold'
+    }
 })
 
 export default ProfileStyles
